c0cb2fd101a52a681a2d73626d5ed408fbeed10e
waylongo <wlwu1993@gmail.com>
2020-01-01 21:06:29 -0600

    update

---
+++ problems/1_two_sum.py
@@ -0,0 +1,18 @@
+# solution 1: brute force
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+# solution 2: one pass hash table
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        dict = {}
+        for i in range(len(nums)):
+            if target - nums[i] in dict:
+                return [dict[target - nums[i]], i]
+            else:
+                dict[nums[i]] = i
