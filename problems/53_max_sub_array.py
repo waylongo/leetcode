6c143637bda4df04eec3ac18090958b626fb5750
waylongo <wlwu1993@gmail.com>
2020-01-07 00:25:16 -0600

    update

---
+++ problems/53_max_sub_array.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        pass
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    print(nums)
+    sol = Solution()
+    res = sol.maxSubArray(nums)
+    print(res)
+
+
+
+
+
